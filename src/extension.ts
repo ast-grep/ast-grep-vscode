90e597124ade576ea40944ef151fbd30f7858117
HerrintonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-11-12 02:04:22 -0500

    feat: add sg lsp skeleton

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,51 @@
-import * as path from 'path'
-import { workspace, ExtensionContext } from 'vscode'
+import { workspace, ExtensionContext, window } from 'vscode'
 
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-  TransportKind
+  Executable
 } from 'vscode-languageclient/node'
 
 let client: LanguageClient
+const diagnosticCollectionName = 'ast-grep-diagnostics'
+const outputChannelName = 'ast-grep'
+const languageClientId = 'ast-grep-client'
+const languageClientName = 'ast-grep language client'
+
+function getExecutable(isDebug: boolean): Executable {
+  return {
+    command: process.env.SERVER_PATH || 'sg',
+    args: ['lsp'],
+    options: {
+      env: {
+        ...process.env,
+        ...(isDebug ? { RUST_LOG: 'debug' } : {})
+      }
+    }
+  }
+}
 
 export function activate(context: ExtensionContext) {
-  // The server is implemented in node
-  let serverModule = context.asAbsolutePath(
-    path.join('server', 'out', 'server.js')
-  )
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] }
-
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
   let serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions
-    }
+    run: getExecutable(false),
+    debug: getExecutable(true)
   }
 
   // Options to control the language client
   let clientOptions: LanguageClientOptions = {
+    diagnosticCollectionName,
     // Register the server for plain text documents
-    documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-    synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-    }
+    documentSelector: [{ scheme: 'file', language: '*' }],
+    outputChannel: window.createOutputChannel(outputChannelName)
   }
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'languageServerExample',
-    'Language Server Example',
+    languageClientId,
+    languageClientName,
     serverOptions,
     clientOptions
   )
