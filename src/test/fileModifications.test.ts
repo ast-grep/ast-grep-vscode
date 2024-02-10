01fd88648aba2c3a371510d11531d1095c1456d1
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-09 23:24:17 -0800

    test: try add more time for diagnistics

--- src/test/fileModifications.test.ts
+++ src/test/fileModifications.test.ts
@@ -7,7 +7,7 @@ import {
   assertDiagnosticsEqual
 } from './utils'
 
-const MAX_WAIT_TIME_FOR_UPDATE = 5000 // ms
+const MAX_WAIT_TIME_FOR_UPDATE = 10000 // ms
 const MAX_WAIT_TIME_FOR_INITIAL_DIAGNOSTICS = 10000 // ms
 
 const docUri = getDocUri('test.ts')
