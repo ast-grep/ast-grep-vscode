db222149baffea41481012486db44c3b3c238201
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-11 00:09:02 -0800

    fix: simplify vscode launch task

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,16 +3,12 @@ import { defineConfig } from 'tsup'
 const isDev = process.env.NODE_ENV !== 'production'
 
 export default defineConfig({
-  entry: [
-    'src/extension.ts',
-    'src/view.ts',
-    ...(isDev ? [] : ['src/webview/index.tsx'])
-  ],
+  entry: ['src/extension.ts', 'src/view.ts', 'src/webview/index.tsx'],
   outDir: 'out',
   sourcemap: isDev ? 'inline' : false,
   clean: !isDev,
   bundle: true,
-  dts: true,
+  dts: false,
   external: ['vscode'],
   env: {
     NODE_ENV: process.env.NODE_ENV ?? 'production'
