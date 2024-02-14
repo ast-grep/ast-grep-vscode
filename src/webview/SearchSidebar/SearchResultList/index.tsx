fd7b1c6a89652306c19153990a8e43a41b9a97ba
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-13 20:24:28 -0800

    feat: optimzie group by impl
    use Map over object https://rehmat-sayany.medium.com/using-map-over-objects-in-javascript-a-performance-benchmark-ff2f351851ed

--- src/webview/SearchSidebar/SearchResultList/index.tsx
+++ src/webview/SearchSidebar/SearchResultList/index.tsx
@@ -8,26 +8,25 @@ type SearchResultListProps = {
   pattern: string
 }
 
-const displayLimit = 2000
-const SearchResultList = ({ matches, pattern }: SearchResultListProps) => {
+function groupBy(matches: SgSearch[]) {
+  const groups = new Map<string, SgSearch[]>()
+  for (const match of matches) {
+    if (!groups.has(match.file)) {
+      groups.set(match.file, [])
+    }
+    groups.get(match.file)?.push(match)
+  }
+  return groups
+}
+
+const SearchResultList = ({ matches }: SearchResultListProps) => {
   const groupedByFile = useMemo(() => {
-    return matches.slice(0, displayLimit).reduce(
-      (groups, match) => {
-        if (!groups[match.file]) {
-          groups[match.file] = []
-        }
-
-        groups[match.file].push(match)
-
-        return groups
-      },
-      {} as Record<string, SgSearch[]>
-    )
+    return groupBy(matches)
   }, [matches])
 
   return (
     <Box mt="10">
-      {Object.entries(groupedByFile).map(([filePath, match]) => {
+      {[...groupedByFile.entries()].map(([filePath, match]) => {
         return <TreeItem filePath={filePath} matches={match} key={filePath} />
       })}
     </Box>
