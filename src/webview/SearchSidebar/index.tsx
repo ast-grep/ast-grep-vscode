71e73ffa7109da74eecbbaf1f3d24b7517d7f258
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-13 22:41:35 -0800

    feat: add loading state
    this will be changed later

--- src/webview/SearchSidebar/index.tsx
+++ src/webview/SearchSidebar/index.tsx
@@ -6,17 +6,25 @@ import { postSearch } from './postMessage'
 import { useState } from 'react'
 import { useDebounce, useLocalStorage } from 'react-use'
 import { UseDarkContextProvider } from './hooks/useDark'
+import { VSCodeProgressRing } from '@vscode/webview-ui-toolkit/react'
 
 const useSearchResult = (inputValue: string) => {
   const [searchResult, setResult] = useState<SgSearch[]>([])
+  const [searching, setSearching] = useState(false)
 
+  // TODO: setSearching has async racing condition here
   const refreshSearchResult = useCallback(() => {
-    postSearch(inputValue).then(setResult)
+    setSearching(true)
+    postSearch(inputValue).then(res => {
+      setResult(res)
+      setSearching(false)
+    })
   }, [postSearch, setResult, inputValue])
 
   useDebounce(refreshSearchResult, 100, [inputValue])
 
   return {
+    searching,
     searchResult,
     refreshSearchResult
   }
@@ -27,7 +35,8 @@ export const SearchSidebar = () => {
     'ast-grep-search-panel-input-value',
     ''
   )
-  const { searchResult, refreshSearchResult } = useSearchResult(inputValue)
+  const { searchResult, refreshSearchResult, searching } =
+    useSearchResult(inputValue)
 
   return (
     <UseDarkContextProvider>
@@ -36,7 +45,11 @@ export const SearchSidebar = () => {
         refreshResult={refreshSearchResult}
         setInputValue={setInputValue}
       />
-      <SearchResultList matches={searchResult} />
+      {searching ? (
+        <VSCodeProgressRing />
+      ) : (
+        <SearchResultList matches={searchResult} />
+      )}
     </UseDarkContextProvider>
   )
 }
