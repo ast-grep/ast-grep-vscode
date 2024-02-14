030392a45d1821e900cd1e0bd8a179e32f29d066
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-14 00:41:03 -0800

    feat: add loading bar

---
+++ src/webview/LoadingBar.tsx
@@ -0,0 +1,28 @@
+const progressBarStyle = {
+  width: '100%',
+  height: '6px',
+  overflow: 'hidden',
+  position: 'relative'
+} as const
+
+interface LoadingBarProps {
+  loading: boolean
+}
+
+export default function LoadingBar({ loading }: LoadingBarProps) {
+  const style = {
+    display: loading ? '' : 'none',
+    position: 'absolute',
+    top: '0'
+  } as const
+  return (
+    <div
+      style={progressBarStyle}
+      role="progressbar"
+      aria-valuemin={0}
+      aria-hidden={loading}
+    >
+      <div className="progressBar" style={style} />
+    </div>
+  )
+}
