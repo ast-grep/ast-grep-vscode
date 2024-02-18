5933197ec220115896e2b3c45f674033c99bdb32
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-17 23:54:01 -0500

    feat: move query debounce to useQuery

--- src/webview/SearchSidebar/SearchWidgetContainer/index.tsx
+++ src/webview/SearchSidebar/SearchWidgetContainer/index.tsx
@@ -1,27 +1,19 @@
-import { useCallback } from 'react'
 import { useSearchQuery } from '../../hooks/useQuery'
 import SearchOptions from './comps/SearchOptions'
 import SearchWidget from './comps/SearchWidget'
 
 interface Props {
-  refreshResult: () => void
   onQueryChange: (query: string) => void
 }
 
-const SearchWidgetContainer = ({ refreshResult, onQueryChange }: Props) => {
-  const { inputValue, setInputValue } = useSearchQuery()
-  const onChange = useCallback(
-    (val: string) => {
-      setInputValue(val)
-      onQueryChange(val)
-    },
-    [onQueryChange, setInputValue]
-  )
+const SearchWidgetContainer = ({ onQueryChange }: Props) => {
+  const { inputValue, setInputValue, refreshResult } =
+    useSearchQuery(onQueryChange)
   return (
     <div>
       <SearchWidget
         inputValue={inputValue}
-        setInputValue={onChange}
+        setInputValue={setInputValue}
         refreshResult={refreshResult}
       />
       <SearchOptions />
--- src/webview/hooks/useQuery.tsx
+++ src/webview/hooks/useQuery.tsx
@@ -1,5 +1,8 @@
+import { useCallback } from 'react'
 import { useLocalStorage } from 'react-use'
-export function useSearchQuery() {
+import { useDebounce } from 'react-use'
+
+export function useSearchQuery(onQueryChange: (query: string) => void) {
   const [inputValue = '', setInputValue] = useLocalStorage(
     'ast-grep-search-panel-input-value',
     ''
@@ -8,10 +11,18 @@ export function useSearchQuery() {
     'ast-grep-search-panel-include-value',
     ''
   )
+  const refreshResult = useCallback(() => {
+    onQueryChange(inputValue)
+  }, [inputValue, onQueryChange])
+
+  // auto refresh result when input value changes
+  useDebounce(refreshResult, 100, [inputValue])
+
   return {
     inputValue,
     setInputValue,
     includeFile,
-    setIncludeValue
+    setIncludeValue,
+    refreshResult
   }
 }
