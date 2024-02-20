7d8d4764cf6e702d06457916f3ae6791f4368761
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-20 15:06:43 -0500

    feat: move button style to stylex

--- src/webview/SearchSidebar/SearchWidgetContainer/comps/SearchOptions.tsx
+++ src/webview/SearchSidebar/SearchWidgetContainer/comps/SearchOptions.tsx
@@ -1,28 +1,28 @@
-import type { CSSProperties } from 'react'
 import { VscEllipsis } from 'react-icons/vsc'
 import IncludeFile from './IncludeFile'
+import * as stylex from '@stylexjs/stylex'
 
-const buttonStyle: CSSProperties = {
-  background: 'transparent',
-  // TODO: add hover effect, may needs esbuild stylex
-  // backgroundColor: 'var(--vscode-toolbar-hoverOutline)',
-  color: 'var(--vscode-foreground)',
-  cursor: 'pointer',
-  width: 'fit-content',
-  alignSelf: 'end',
-  textAlign: 'end',
-  padding: '0 4px',
-  height: '16px',
-  flex: '0 0 auto',
-  position: 'absolute',
-  top: '0',
-  right: '-2px'
-}
-const optionsStyle: CSSProperties = {
-  minHeight: '16px',
-  marginLeft: '18px',
-  position: 'relative'
-}
+const styles = stylex.create({
+  button: {
+    background: 'transparent',
+    color: 'var(--vscode-foreground)',
+    cursor: 'pointer',
+    width: 'fit-content',
+    alignSelf: 'end',
+    textAlign: 'end',
+    padding: '0 4px',
+    height: '16px',
+    flex: '0 0 auto',
+    position: 'absolute',
+    top: '0',
+    right: '-2px'
+  },
+  options: {
+    minHeight: '16px',
+    marginLeft: '18px',
+    position: 'relative'
+  }
+})
 
 interface SearchOptionsProps {
   includeFile: string
@@ -40,8 +40,8 @@ export default function SearchOptions({
   toggleOptions
 }: SearchOptionsProps) {
   return (
-    <div style={optionsStyle}>
-      <button style={buttonStyle} onClick={toggleOptions}>
+    <div {...stylex.props(styles.options)}>
+      <button {...stylex.props(styles.button)} onClick={toggleOptions}>
         <VscEllipsis />
       </button>
       {showOptions && (
