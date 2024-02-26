4947551a299a875a4b7098a81c47ca9e77fc437f
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-02-26 01:09:15 -0800

    feat: tweak style

--- src/webview/SearchSidebar/SearchResultList/Actions.tsx
+++ src/webview/SearchSidebar/SearchResultList/Actions.tsx
@@ -1,12 +1,20 @@
+import * as stylex from '@stylexjs/stylex'
 import { VscReplace } from 'react-icons/vsc'
+
+const styles = stylex.create({
+  list: {
+    listStyle: 'none',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+})
+
 export function Actions() {
   return (
-    <ul role="toolbar">
+    <ul {...stylex.props(styles.list)} role="toolbar">
       {/* VSCode supports shortcut Replace (⇧⌘1)*/}
-      <li role="presentation" title="Replace">
-        <a role="button" aria-label="Replace" tabIndex={0}>
-          <VscReplace />
-        </a>
+      <li role="button" title="Replace" tabIndex={0}>
+        <VscReplace />
       </li>
     </ul>
   )
